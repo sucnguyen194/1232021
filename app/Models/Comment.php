4399699fb215633b8ec5b5566e2340379b4545c6
sucnguyen194 <58970838+sucnguyen194@users.noreply.github.com>
2021-03-15 12:02:24 +0700

    Revert "Revert "11""
    This reverts commit 005783950a3ffb715e17ab441bf52f31e3327295.

---
+++ app/Models/Comment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    public function comment(){
+        return $this->morphTo();
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function admin(){
+        return $this->belongsTo(User::class,'admin_id');
+    }
+}
